0868d8d7f6a5e17d919924ba31ab8316471427be
Muhammad Ifdal <m_afdal@mhs.unsyiah.ac.id>
2022-05-24 16:48:14 +0700

    Perbaharui halaman login

--- loginV2.php
+++ loginV2.php
@@ -55,7 +55,7 @@ if (isset($_POST["submit"])) {
     <meta charset="utf-8" />
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
-    <title>Login</title>
+    <title>LOGIN</title>
     <meta content="" name="description" />
     <meta content="" name="keywords" />
 
